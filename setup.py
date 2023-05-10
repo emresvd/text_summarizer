38a8ed5eef3a3e4ef20806648a2cc935cb84d0dd
emresvd <109899306+emresvd@users.noreply.github.com>
2023-05-10 19:05:21 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt", "r") as fh:
+    requirements = fh.read().splitlines()
+
+setup(
+    name='text_summarizer',
+    version='0.0.1',
+    description='',
+    long_description="",
+    author='emresvd',
+    license='MIT',
+    url='https://github.com/emresvd/text_summarizer',
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        "Operating System :: OS Independent",
+    ],
+)
